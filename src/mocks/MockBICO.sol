daea9d66f183fb0833b5aafab7832a80c7a92c42
Aaron Foster <50079365+pythonpete32@users.noreply.github.com>
2022-08-25 02:12:01 +0400

    fix over/underflow error

--- src/mocks/MockBICO.sol
+++ src/mocks/MockBICO.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract MockBICO is ERC20 {
-    constructor() ERC20("BICO", "BICO", 18) {}
+    constructor() ERC20("BICO", "BICO", 18) {
+        _mint(msg.sender, 10000000000 * 10**18);
+    }
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
